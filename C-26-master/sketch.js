a15b36468b3b03986c7494da1f0aba3e327c6f70
yoyorttt <77925727+yoyorttt@users.noreply.github.com>
2021-08-30 13:00:40 +0530

    Add files via upload

---
+++ C-26-master/sketch.js
@@ -0,0 +1,21 @@
+// hello everyone!
+//welcome to masterchef junior. today we will learn to make chocolate cake!
+// so, let's get started!!!
+
+// ¼ cup all-purpose flour
+// cup white sugar
+// tablespoons unsweetened cocoa powder
+// teaspoon baking soda
+// teaspoon salt
+//  tablespoons milk
+
+// Step 1
+//Mix flour, sugar, cocoa powder, baking soda, and salt in a large microwave-safe mug; stir in milk, canola oil, water, and vanilla extract.
+
+//Step 2
+//Cook in microwave until cake is done in the middle, about 1 minute 45 seconds.
+
+// i hope you all enjoyed!
+
+// see you next time!!!!!!!!!
+ 
